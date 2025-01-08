c646a5f42ab905a8f3f5f3f604581ec7cacf6e96
Raiyan <raiyanriz11@gmail.com>
2025-01-08 20:51:42 +0400

    initial commit

---
+++ src/components/habit-stats.tsx
@@ -0,0 +1,73 @@
+import { useSelector, useDispatch } from "react-redux"; 
+import { fetchHabits, Habit } from "../store/habit-slice";
+import { AppDispatch, RootState } from "../store/store";
+import { useEffect } from "react";
+import { LinearProgress, Paper, Typography } from "@mui/material";
+
+function HabitStats() {
+    const {habits,isLoading,error} = useSelector((state: RootState)=>state.habits)//{habits} destructure
+    const dispatch = useDispatch<AppDispatch>();
+
+    useEffect(()=>{
+        dispatch(fetchHabits);
+    },[])
+
+    if(isLoading){
+        return <LinearProgress/>
+    }
+    if(error){
+        return <Typography color="error">{error}</Typography>
+    }
+
+    const getCompletedToday = () => {
+        const today = new Date().toISOString().split("T")[0];
+
+        return habits.filter((habit)=>{
+            habit.completedDates.includes(today);
+        }).length
+    }
+
+    const getStreak = (habit: Habit) => {
+        let streak = 0;
+        const currentDate = new Date();
+
+        while(true){
+            const dateString = currentDate.toISOString().split("T")[0];
+
+            if(habit.completedDates.includes(dateString)){
+                streak++;
+                currentDate.setDate(currentDate.getDate()-1);
+            }else{
+                break;
+            }
+        }
+    return streak;
+    }
+
+    const getLongestStreak = () => {
+        //spread habits arr. replace elements with streak. get max
+        return Math.max(...habits.map(getStreak));
+    }
+
+    return (
+        <Paper elevation={2} sx={{p: 2, mt: 4}}>
+            <Typography variant="h6" gutterBottom>
+                Habit Statistics
+            </Typography>
+            <Typography variant="body1">
+                Total Habits:{habits.length}
+            </Typography>
+            <Typography variant="body1">
+                Completed Today: {getCompletedToday()}
+            </Typography>
+            <Typography variant="body1">
+                Longest Streak: {getLongestStreak()}
+            </Typography>
+        </Paper>
+
+    )
+  
+
+}
+
+export default HabitStats
---
+++ src/store/habit-slice.ts
@@ -0,0 +1,114 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+export interface Habit{
+    id: string;
+    name: string;
+    frequency: "daily" | "weekly";
+    completedDates: string[];
+    createdAt: string;
+}
+
+interface HabitState {
+    habits: Habit[];
+    isLoading: boolean;
+    error: string | null;
+}
+
+const initialState: HabitState = {
+    habits: [],
+    isLoading: false,
+    error: null,
+};
+
+export const fetchHabits = createAsyncThunk("habits/fetchHabits", async () => {
+    // Simulating an API call
+    await new Promise((resolve) => setTimeout(resolve, 1000));// 1s timeout
+    const mockHabits: Habit[] = [
+      {
+        id: "1",
+        name: "Gym",
+        frequency: "daily",
+        completedDates: [],
+        createdAt: new Date().toISOString(),
+      },
+      {
+        id: "2",
+        name: "Read",
+        frequency: "daily",
+        completedDates: [],
+        createdAt: new Date().toISOString(),
+      },
+    ];
+    return mockHabits;
+  });
+
+const habitSlice = createSlice({
+    name: "habits",
+    initialState,
+    reducers: {
+        addHabit: (
+            state,
+            action: PayloadAction<{name: string;frequency: "daily" | "weekly"}>
+        ) => {
+                const newHabit: Habit = {
+                    id: Date.now().toString(),
+                    name: action.payload.name,
+                    frequency: action.payload.frequency,
+                    completedDates: [],
+                    createdAt: new Date().toISOString(),
+                }
+                state.habits.push(newHabit);
+        },
+        toggleHabit: (
+            state,
+            action: PayloadAction<{id: string; date: string }>
+        ) => {
+            //check if habit is in habits list
+            const habit = state.habits.find((h)=>h.id==action.payload.id)
+
+            if(habit){
+                // check if date when toggled is present in a specific habit.
+                const index = habit.completedDates.indexOf(action.payload.date)
+                if(index>-1){
+                    // remove specific date from completed dates
+                    habit.completedDates.splice(index,1);
+                }else{
+                    habit.completedDates.push(action.payload.date);
+                }
+            }
+        },
+        removeHabit: (
+            state,
+            action: PayloadAction<{id: string}>
+        ) => {
+            const habit = state.habits.find((h)=>h.id==action.payload.id)
+            if(habit){
+                const index = state.habits.indexOf(habit);
+                if(index>-1){
+                    state.habits.splice(index,1);
+                }
+            }
+            
+        },
+    },
+    extraReducers: (builder) => {
+        // while fetching is taking place
+        builder.addCase(fetchHabits.pending, (state)=>{
+            state.isLoading = true
+        })
+        // fetching is complete
+        builder.addCase(fetchHabits.fulfilled,(state,action)=>{
+            state.isLoading = false;
+            state.habits = action.payload;
+        })
+        // fetching failed
+        builder.addCase(fetchHabits.rejected,(state,action)=>{
+            state.isLoading = false;
+            state.error = action.error.message || "failed to fetch habits";
+        })
+    },
+});
+
+export const {addHabit,toggleHabit,removeHabit} = habitSlice.actions;
+
+export default habitSlice.reducer;
